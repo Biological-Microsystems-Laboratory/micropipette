284b5bdcf40522acead962429dc14e003cddc381
mbrenn3 <mbrenn3@uic.edu>
2015-06-02 15:23:31 -0500

    mean std for comparison

--- data/printed-Vs-commercial/analysis.m
+++ data/printed-Vs-commercial/analysis.m
@@ -8,3 +8,15 @@ load commercial-pipette.csv
 mean20 = mean(mean(X20uL_printed_pipette,1),2)
 mean50 = mean(mean(X50uL_printed_pipette,1),2)
 mean200 = mean(mean(X200uL_printed_pipette,1),2)
+
+std20 = std(mean(X20uL_printed_pipette,1))
+std50 = std(mean(X50uL_printed_pipette,1))
+std200 = std(mean(X200uL_printed_pipette,1))
+
+commercial_mean_20=mean(commercial_pipette(:,1))
+commercia_mean_l50=mean(commercial_pipette(:,2))
+commercial_mean_200=mean(commercial_pipette(:,3))
+
+commercial_std_20=std(commercial_pipette(:,1))
+commercia_std_l50=std(commercial_pipette(:,2))
+commercial_std_200=std(commercial_pipette(:,3))
